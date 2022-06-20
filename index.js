b09630454f7cb2012465a7682b8804ac17e4e25f
Mo Kakwan <wavewash@gmail.com>
2022-06-20 17:45:01 -0400

    updates the data correctly

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
-
+const dotenv = require("dotenv")
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
+
+dotenv.config()
 
 var states = [];
 
@@ -15,15 +17,32 @@ app.get("/clickstate", (req, res) => {
 app.use("/", express.static(__dirname + "/public"));
 
 server.listen(process.env.PORT);
+console.log("Your PORT via ENV:" + process.env.PORT);
 
 io.on("connection", (socket) => {
   console.log("Connected " + socket.id);
-  let id = socket.id;
+  console.log(states);
+  sIndex = states.findIndex(s => s.id == socket.id);
+  if(sIndex == -1) { //don't dupe existing sockets
+    states.push({id:socket.id})
+  }
   io.emit("getState", states);
 
   socket.on("distributeState", (state) => {
-    console.log("distributeState!");
-    states.push(JSON.stringify(state));
+    sIndex = states.findIndex(s => s.id == state.id);
+    console.log("distributeState!" + JSON.stringify(state) + " " + state.id + " " + sIndex);
+    if(sIndex >= 0) {
+      states[sIndex] = (state);
+    }
+    io.emit("getState", states);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("disconnected " + socket.id);
+    sIndex = states.findIndex(s => s.id == socket.id);
+    if(sIndex >= 0) {
+      states.splice(sIndex, 1);
+    }
     io.emit("getState", states);
   });
 
--- public/client.js
+++ public/client.js
@@ -1,16 +1,4 @@
-const serverUrl = "https://tr5qqn.sse.codesandbox.io/";
-
-const socket = new io.Socket();
-socket.on("getState", (states) => {
-  console.log("getState");
-  statedisplay.innerHTML = "";
-  states.forEach((message) => {
-    const messageItem = document.createElement("li");
-    messageItem.innerText = message;
-    statedisplay.appendChild(messageItem);
-  });
-});
-//socket.connect(serverUrl);
+const serverUrl = "http://localhost:3000";
 
 const myid = document.getElementById("myid");
 const statedisplay = document.getElementById("statedisplay");
@@ -18,13 +6,35 @@ const stateInput = document.getElementById("message-input");
 const sendForm = document.getElementById("send-form");
 const clearBtn = document.getElementById("clearBtn");
 
+function clearAndRender(states) {
+  statedisplay.innerHTML = "";
+  states.forEach((message) => {
+    const messageItem = document.createElement("li");
+    messageItem.innerText = JSON.stringify(message);
+    statedisplay.appendChild(messageItem);
+  });
+}
+
+const socket = new io(serverUrl);
+socket.on("getState", (states) => {
+  console.log(states);
+  clearAndRender(states);
+});
+
+
+
+socket.on("connect", () => {
+  console.log("connected");
+  myid.textContent = socket.id;
+})
+
+
 sendForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
   const state = { id: socket.id, value: stateInput.value };
   socket.emit("distributeState", state);
 
-  myid.textContent = socket.id;
   stateInput.value = "";
 });
 
@@ -34,13 +44,9 @@ clearBtn.addEventListener("click", (e) => {
 });
 
 window.onload = function () {
-  fetch(serverUrl)
+  fetch(serverUrl + "/clickstate")
     .then((res) => res.json())
-    .then((clickerstates) => {
-      clickerstates.forEach((cstate) => {
-        const cstateItem = document.createElement("li");
-        cstateItem.innerText = cstate;
-        statedisplay.appendChild(cstateItem);
-      });
+    .then((states) => {
+      clearAndRender(states);
     });
 };
