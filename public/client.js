0a1d6a4afba3500af1c97698bb75ce797976ab00
Glitch (puffy-abiding-millennium) <none>
2022-06-20 18:38:36 +0000

    📗👜 Updated with Glitch

---
+++ public/client.js
@@ -0,0 +1,34 @@
+const socket = io.connect("https://puffy-abiding-millennium.glitch.me");
+
+const statedisplay = document.getElementById("statedisplay");
+const stateInput = document.getElementById("message-input");
+const sendButton = document.getElementById("send-button");
+
+var id;
+
+socket.on("getState", message => {
+  const messageItem = document.createElement("li");
+  messageItem.innerText = message;
+  statedisplay.appendChild(messageItem);
+});
+
+socket.on("registerId", myId => {
+  id = myId;
+})
+
+sendButton.addEventListener("click", () => {
+  const state = {"id":id, "value":stateInput.value};
+  socket.emit("distributeState", state);
+});
+
+window.onload = function() {
+  fetch("https://puffy-abiding-millennium.glitch.me/clickstate")
+    .then(res => res.json())
+    .then(clickerstates => {
+      clickerstates.forEach(cstate => {
+        const cstateItem = document.createElement("li");
+        cstateItem.innerText = cstate;
+        statedisplay.appendChild(cstateItem);
+      });
+    });
+};
