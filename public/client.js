73cf1bc86557a09c802cc0d94578f3f11226c622
Mo Kakwan <wavewash@yahoo.com>
2022-06-20 18:10:48 -0400

    Update client.js

--- public/client.js
+++ public/client.js
@@ -1,4 +1,4 @@
-const serverUrl = "http://localhost:3000";
+const serverUrl = "https://clicky-backend.herokuapp.com/";
 
 const myid = document.getElementById("myid");
 const statedisplay = document.getElementById("statedisplay");
